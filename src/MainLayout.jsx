cc667e90908ea4b5bc1a1255910e38ceb0cde7dc
mahadinrahman <mahadinrahman333@gmail.com>
2026-04-17 20:31:55 +0600

    loading added

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -5,17 +5,20 @@ import Home from './Components/Home/Home';
 import Timeline from './Components/Timeline/Timeline';
 import Stats from './Components/Stats/Stats';
 import Footer from './Components/Footer';
+import { Suspense } from 'react';
 
 const MainLayout = () => {
     return (
         <div>
            <Navbar></Navbar>
 
+           <Suspense fallback={<span className="loading loading-spinner text-primary mt-5 ml-50 md:ml-150"></span>}>
            <Outlet>
            <Home></Home>
            <Timeline></Timeline>
            <Stats></Stats>
            </Outlet>
+           </Suspense>
            
            <Footer></Footer>
         </div>
